a9b8b59173dea443c5abb207d8699177b2e3f686
phlyne <stokolvf316@gmail.com>
2020-10-06 00:07:24 +0300

    last version + tests

--- currency.py
+++ currency.py
@@ -1,51 +1,35 @@
+from datetime import datetime
 import urllib.request
 import sys
-import time
 
 
-def leap_year_check(year):
-    if year % 4 == 0 and (year % 400 == 0 or year % 100 != 0):
-        return True
-    else:
+def EXIT(message, exit_code):
+    print(message)
+    sys.exit(exit_code)
+
+
+def date_check(date):    #we just need to check if date in range [1992-07-01;<today>] dmy
+
+    year = int(datetime.today().strftime("%Y"))
+    month = int(datetime.today().strftime("%m"))
+
+    if int(date[0]) > int(datetime.today().strftime("%d")) and int(date[2]) == year and int(date[1]) == month:
         return False
 
-
-def days_in_month(month, year):
-    if month == 2:
-        if leap_year_check(year):
-            return 29
-        else:
-            return 28
-    if 1 <= month <= 7:
-        if month % 2 == 0:
-            return 30
-        else:
-            return 31
-    elif 7 < month <= 12:
-        if month % 2 == 0:
-            return 31
-        else:
-            return 30
-    else:
+    if (int(date[1]) > month and int(date[2]) == year) or (int(date[2]) == 1992 and int(date[1]) < 7):
         return False
 
-
-def date_check(date):
-    date = date.split("-")
-    if len(date) == 3:
-        date[0] = int(date[0])
-        date[1] = int(date[1])
-        date[2] = int(date[2])
-        if date[0] // 1000 == 0 or date[0] > time.time() // 31536000 + 1970 or date[0] <= 1991:
-            return False
-        if days_in_month(date[1], date[0]) == False or (date[0] == 1992 and date[1] < 7) or (int(time.strftime("%D", time.localtime())[0:2]) < date[1]):
-            return False
-        if days_in_month(date[1], date[0]) < date[2] or date[2] < 0 or (int(time.strftime("%D", time.localtime())[3:5]) < date[2]):
-            return False
-    else:
+    if int(date[2]) < 1992 or int(date[2]) > year:
         return False
+
     return True
 
+
+EXIT_SUCCESS = 0
+CURRENCY_ERROR = 1
+DATE_ERROR = 2
+EXIT_REFERENCE = 3
+INPUT_ERROR = 4
 
 if len(sys.argv) == 1 or sys.argv[1] == "-h" or sys.argv[1] == "-a":
     print(
@@ -54,30 +38,38 @@ if len(sys.argv) == 1 or sys.argv[1] == "-h" or sys.argv[1] == "-a":
         "this script has data from 1992-07-01", "\n",
         "other arguments: [-h] [-a] [-ec] [-exit_codes]", sep=""
           )
-    sys.exit("exit code: 3")
-elif sys.argv[1] == "-exit_codes" or sys.argv[1] == "-ec":
+    EXIT("", EXIT_REFERENCE)
+elif sys.argv[1] == "--exit-codes" or sys.argv[1] == "-e":
     print(
         "exit code 0: success", "\n",
         "exit code 1: currency-code format error", "\n",
         "exit code 2: date error(not in data base or date in future)", "\n",
         "exit code 3: references", sep=""
     )
-    sys.exit("exit code: 3")
-currency = sys.argv[1]
-date = sys.argv[2]  # y-m-d
+    EXIT("", EXIT_REFERENCE)
+
+if len(sys.argv) != 3:
+    EXIT("Input should have format: <currency code> <date>.", INPUT_ERROR)
+
+try:
+    datetime.strptime(sys.argv[2], "%Y-%m-%d")
+except ValueError:
+    EXIT("Date format error. Date should have format: yyyy-mm-dd.", DATE_ERROR)
+else:
+    date = sys.argv[2].split("-")
+    date[0], date[2] = date[2], date[0]
+
+currency = sys.argv[1].upper()
+
 if date_check(date):
-    lst = date.split("-")  # d/m/y
-    lst[0], lst[2] = lst[2], lst[0]  # swap day and year for cbr format
-    date = "/".join(lst)  # date for cbr
+    date = "/".join(date)  # date for cbr
     url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date
     html = str(urllib.request.urlopen(url).read())
-    if currency.upper() in html:
-        print(f"Курс рубля к {currency.upper()} на {date}: "
-              f"{html[html.find('Value', html.find(currency.upper())) + 6:html.find('Value',html.find(currency.upper())) + 13]}")
-        sys.exit("exit code: 0")
+    if currency in html:
+        EXIT(f"Курс рубля к {currency} на {date}: "
+             f"{html[html.find('Value', html.find(currency)) + 6:html.find('Value',html.find(currency)) + 13]}",
+             EXIT_SUCCESS)
     else:
-        print(f"error: currency({currency}) not found")
-        sys.exit("exit code: 1")
+        EXIT(f"error: currency({currency}) not found", CURRENCY_ERROR)
 else:
-    print("error: date input")
-    sys.exit("exit code: 2")
+    EXIT("error: date input", DATE_ERROR)
