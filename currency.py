fb31b54827197ceeac9be74a6be94e021bd827b4
phlyne <stokolvf316@gmail.com>
2020-10-11 01:54:26 +0300

    version with date_check and try fixes

--- currency.py
+++ currency.py
@@ -8,22 +8,12 @@ def EXIT(message, exit_code):
     sys.exit(exit_code)
 
 
-def date_check(date):    #we just need to check if date in range [1992-07-01;<today>] dmy
+def date_check(dateinp):
+    min_date = datetime(1992, 7, 1)
+    max_date = datetime.today()
 
-    lst[0] = int(lst[0])
-    lst[1] = int(lst[1])
-    lst[2] = int(lst[2])
-    today = datetime.today()
-
-    if date[0] > today.day and date[2] == today.year and date[1] == today.month:
+    if min_date >= dateinp or max_date <= dateinp:
         return False
-
-    if (date[1] > today.month and date[2] == today.year) or (date[2] == 1992 and date[1] < 7):
-        return False
-
-    if date[2] < 1992 or date[2] > today.year:
-        return False
-
     return True
 
 
@@ -54,23 +44,19 @@ if len(sys.argv) != 3:
     EXIT("Input should have format: <currency code> <date>.", INPUT_ERROR)
 
 try:
-    datetime.strptime(sys.argv[2], "%Y-%m-%d")
+    dateinp = datetime.strptime(sys.argv[2], "%Y-%m-%d")
 except ValueError:
     EXIT("Date format error. Date should have format: yyyy-mm-dd.", DATE_ERROR)
-else:
-    date = sys.argv[2].split("-")
-    date[0], date[2] = date[2], date[0]
 
 currency = sys.argv[1].upper()
 
-lst = date.copy()
-
-if date_check(lst):
-    date = "/".join(date)  # date for cbr
-    url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date
+if date_check(dateinp):
+    dateinp = datetime.strftime(dateinp, "%d/%m/%Y")
+    url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + dateinp
     html = str(urllib.request.urlopen(url).read())
+
     if currency in html:
-        EXIT(f"Курс рубля к {currency} на {date}: "
+        EXIT(f"Курс рубля к {currency} на {dateinp}: "
              f"{html[html.find('Value', html.find(currency)) + 6:html.find('Value',html.find(currency)) + 13]}",
              EXIT_SUCCESS)
     else:
